25f0ecc90c311114441d880d16a322f23f84172d
CD-Z <69157453+CD-Z@users.noreply.github.com>
2026-07-19 15:15:05 +0200

    update packages

--- metro.config.js
+++ metro.config.js
@@ -1,54 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+const config = getDefaultConfig(__dirname);
 
-const path = require('path');
-const fs = require('fs');
-const defaultConfig = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push('sql');
 
-const map = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-};
-const customConfig = {
-  resolver: {
-    sourceExts: [...defaultConfig.resolver.sourceExts, 'sql'],
-  },
-  server: {
-    port: 8081,
-    enhanceMiddleware: (metroMiddleware, metroServer) => {
-      return (request, res, next) => {
-        const filePath = path.join(
-          __dirname,
-          'android/app/src/main',
-          request._parsedUrl.path || '',
-        );
-        const ext = path.parse(filePath).ext;
-        if (fs.existsSync(filePath)) {
-          try {
-            const data = fs.readFileSync(filePath);
-            res.setHeader('Content-type', map[ext] || 'text/plain');
-            res.end(data);
-          } catch (err) {
-            res.statusCode = 500;
-            res.end(`Error getting the file: ${err}.`);
-          }
-        } else {
-          return metroMiddleware(request, res, next);
-        }
-      };
-    },
-  },
-};
-module.exports = mergeConfig(defaultConfig, customConfig);
+module.exports = config;
